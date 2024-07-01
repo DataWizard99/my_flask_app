5b1b0e9b15ce0138bfce40376dd5eb5d33af3eab
DataWizard99 <163642480+DataWizard99@users.noreply.github.com>
2024-07-01 13:30:25 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import spacy
+from spacy import displacy
+
+# Load the spaCy model
+nlp = spacy.load('en_core_web_sm')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/entity', methods=['POST', 'GET'])
+def entity():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            readable_file = file.read().decode('utf-8', errors='ignore')
+            docs = nlp(readable_file)
+            html = displacy.render(docs, style='ent')
+            return render_template('index.html', html=html, text=readable_file)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
